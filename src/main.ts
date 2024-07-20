08ad722bfc4c266ee229912bc4a9e80ddcdbbd1a
开坦克的贝塔 <82384326+goodpersons@users.noreply.github.com>
2024-07-20 19:23:59 +0800

    postcss 处理移动端适配

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,14 @@ createApp(App)
   .use(router)
   .mount('#app')
 
+  // 设计稿 宽度
+const rootValue = 16;
+const rootWidth = 375;
+//  客户端 宽度
+const deviceWidth = document.documentElement.clientWidth;
+//  计算 1rem 的值
+const rem = deviceWidth / rootWidth * rootValue;
+document.documentElement.style.fontSize = rem + 'px';
+
+
+
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <div>首页</div>
+  <div class="test">我我我我我我我我我我</div>
 </template>
 
 <script setup lang='ts'>
 import { featchTest } from '../../api/test'
-import { onMounted } from 'vue'
 featchTest()
 
 </script>
 
 <style scoped>
-
+.test {
+  font-size: 37px;
+}
 </style>
