4e2fad318592f0c350df4fee2d7668ba2e73426d
开坦克的贝塔 <82384326+goodpersons@users.noreply.github.com>
2024-07-21 19:56:40 +0800

    sass 导入& 全局引用配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
@@ -6,14 +7,27 @@ import { VantResolver } from '@vant/auto-import-resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [
+    vue(),
     AutoImport({
       resolvers: [VantResolver()],
     }),
     Components({
       resolvers: [VantResolver()],
-    })
+    }),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/styles/global.scss";`,
+      },
+    },
+  },
   //  vite 配置反向代理
   server: {
     port: 3000,
